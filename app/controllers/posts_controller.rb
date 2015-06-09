36a61fd8f0fbc596008801e2ac020a5f9d5df2c7
Rickard Sunden <rickard.sunden@outlook.com>
2015-06-09 09:58:32 -0400

    Added more views, all posts functionality, routes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,19 @@
 class PostsController < ApplicationController
-  def view
-    #erb :view
-    #render :view
+  def show
+    #erb :show
+    #render :show
     @post = Post.find(params[:id])
+  end
+
+  def index
+    page = params[:page] || 1
+    @posts = self.get_page(page)
+  end
+
+  protected
+  def get_page(n)
+    page_offset = (n-1) * 10
+    page_size = 10
+    Post.order(written_at: :desc).offset(page_offset).limit(page_size)
   end
 end
